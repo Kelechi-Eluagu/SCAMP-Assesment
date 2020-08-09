b89a6da5d239acc57bfb586f9ec209a3b081fdd1
Kelechi-Eluagu <kellyeluagu@gmail.com>
2020-08-09 20:19:55 +0100

    work in progress

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var i;
-var fib = []; // Initialize array!
+var num;
+var fib = []; 
 
 fib[0] = 0;
 fib[1] = 1;
-for (i = 2; i <= 10; i++) {
-  // Next fibonacci number = previous + one before previous
-  // Translated to JavaScript:
-  fib[i] = fib[i - 2] + fib[i - 1];
-  console.log(fib[i]);
+
+for (num = 2; num <= 10; num++) {
+  fib[num] = fib[num - 2] + fib[num - 1];
+  console.log(fib[num]);
 }
